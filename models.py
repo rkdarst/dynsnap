749aeafe243cc60f29cd0cf38386e969e6ab295b
Richard Darst <rkd@zgib.net>
2014-12-17 14:01:47 +0200

    models: make models iterators, not functions that print.

--- models.py
+++ models.py
@@ -5,16 +5,33 @@ import random
 
 rand = lambda: random.uniform(0, 1)
 
+def group_by_t(it):
+    """Transform events iterator to group format.
+
+    This transforms an iterator over (t, e) pairs to (t, [e0, e1, e2,
+    ...])."""
+    last_t = None
+    last_events = [ ]
+    for t, e in it:
+        if t != last_t:
+            yield last_t, last_events
+            last_t = t
+            last_events = [ ]
+        last_events.append(e)
+    yield last_t, last_events
+
 def toy101(args):
     for t in range(0, 100):
         events = [0, 1, 2, 3, 4, 5]
-        print t*100, " ".join(str(x) for x in sorted(events))
+        for e in sorted(events):
+            yield t, e
 
 def toy102(args):
     for t in range(0, 100):
         generation = t // 10
         events = range(generation*6, (generation+1)*6)
-        print t*100, " ".join(str(x) for x in sorted(events))
+        for e in sorted(events):
+            yield t, e
 
 def toy103(args):
     n = 50
@@ -23,11 +40,11 @@ def toy103(args):
         generation = t // 10
         events = range(generation*n, (generation+1)*n)
         events2 = random.sample(events, s)
-        print t*100, " ".join(str(x) for x in sorted(events2))
+        for e in sorted(events2):
+            yield t, e
 
 
 def periodic(args):
-
     p = .5   # Activated edges exist exist with this probability
     q = .2   # Fraction of activated edges
     c_scale = .02  # How many edges change per step?
@@ -45,7 +62,7 @@ def periodic(args):
         # build up all time scales.
         for e in events:
             if rand() < edge_p[e]:
-                print t, e
+                yield t, e
 
         # Randomly perturb some edges
         c = c_min + c_scale * (.5 - .5 * math.cos(2*math.pi * t / tau))
@@ -66,7 +83,17 @@ if __name__ == "__main__":
     parser.add_argument("--q", type=float, default=.2)
     parser.add_argument("--tau", type=int)
     parser.add_argument("--c_scale", type=int)
+    parser.add_argument("--grouped", action='store_true', help="group output by time")
     args = parser.parse_args()
 
 
-    globals()[args.model](args)
+
+    it = globals()[args.model](args)
+
+    if args.grouped:
+        it = group_by_t(it)
+        for t, events in it:
+            print t, " ".join(str(x) for x in events)
+    else:
+        for t, e in it:
+            print t, e
