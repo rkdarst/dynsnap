b2f1ccbc4cf6435fec645a675d93dd04be1a1e8b
Richard Darst <rkd@zgib.net>
2014-12-18 10:54:39 +0200

    tests: Change to class-based system

--- tests.py
+++ tests.py
@@ -7,83 +7,100 @@ import dynsnap
 import models
 
 
-def run(name, model, output, modelargs={}, kwargs={}):
+class BaseTest(object):
+    #m = models.model_func
+    #ma = {'arg': 'value' }
+    #desc = 'description for under subplot'
+    ann_pk = True   # Annotate the peaks in the output plot?
 
-    events = dynsnap.Events()
-    events.add_events((t, e, 1) for t,e in model(**modelargs))
 
-    finder = dynsnap.SnapshotFinder(events,
-                                    weighted=modelargs.get('w'))
+    def run(self, output, plot=True):
+        """Do a run and plot results."""
+        # sinec self.m is turned into a bound method.  I don't want to
+        # decorate every single subclass with staticmethod... is there
+        # a better way to handle this?
+        model = self.m.im_func
 
-    if 'plot' in kwargs:
-        plotter = dynsnap.Plotter(finder,
-                  args=dict(annotate_peaks=kwargs.get('annotate_peaks', True)))
-    while True:
-        interval = finder.find()
-        if interval is None: break
-        t1, t2 = interval
+        events = dynsnap.Events()
+        events.add_events((t, e, 1) for t,e in model(**self.ma))
 
-        if 'plot' in kwargs:
-            plotter.add(finder)
+        finder = dynsnap.SnapshotFinder(events,
+                                        weighted=self.ma.get('w'))
 
-        print t1, t2
+        if plot:
+            plotter = dynsnap.Plotter(finder,
+                      args=dict(annotate_peaks=self.ann_pk))
+        # This is the core.  Iterate through until we are done wwith
+        # all intervals.
+        while True:
+            interval = finder.find()
+            if interval is None: break
+            t1, t2 = interval
 
-    if 'plot' in kwargs:
-        def cb(lcls):
-            title = name
-            if modelargs:
-                title = "%s (%s)"%(title, " ".join("%s=%s"%(k,v)
-                            for k,v in sorted(modelargs.iteritems())))
-            if 'desc' in kwargs:
-                title += '\n'+kwargs['desc']
-            lcls['fig'].suptitle(title)
+            if plot:
+                plotter.add(finder)
 
-        plotter.plot(output, callback=cb)
+            print t1, t2
 
+        if plot:
+            # Callback function to decorate the plot a little bit.
+            def cb(lcls):
+                title = self.__class__.__name__
+                if self.ma:
+                    title = "%s (%s)"%(title, " ".join("%s=%s"%(k,v)
+                                for k,v in sorted(self.ma.iteritems())))
+                if 'desc' in kwargs:
+                    title += '\n'+kwargs['desc']
+                lcls['fig'].suptitle(title)
+
+            plotter.plot(output, callback=cb)
+
+
+T = BaseTest
+
+class toy101A(T): m=models.toy101
+class toy102A(T): m=models.toy102
+class toy103A(T):
+    m=models.toy103; ma={'seed':13}
+class toy103B(T):
+    m=models.toy101; ma={'seed':18}
+    desc="has some size-8 intervals"
+
+class toy103N(T):
+    m=models.toy101; ma={'seed':15}
+    desc="upper bound too high"
+
+class periodic1A(T):
+    m=models.periodic; ma={'N':1000, 'seed':13}; ann_pk=False
+    desc='periodic'
+class periodic1Aw(T):
+    m=models.periodic; ma={'N':1000, 'seed':13, 'w':True}; ann_pk=False
+    desc='periodic - weighted'
+
+
+all_tests = [x for name, x in globals().items()
+             if isinstance(x, type) and issubclass(x, BaseTest) and x != BaseTest
+             and not name.startswith('_') ]
 
 if __name__ == '__main__':
 
     out_path = 'out-tests/'
     to_run = sys.argv[1:]
-    kwargs_ = dict(plot=True)
+    kwargs = dict(plot=True)
 
-    tests = [
-        ('toy101A', models.toy101),
-        ('toy102A', models.toy102),
-        ('toy103A', models.toy103, dict(seed=13)),
-        ('toy103B', models.toy103, dict(seed=18,),
-                                   dict(desc="has some size-8 intervals")),
-
-        ('toy103N', models.toy103, dict(seed=15),
-                                   dict(desc="upper bound too high")),
-
-
-        ('periodic1A', models.periodic, dict(N=1000, seed=13),
-                                        dict(desc="periodic",
-                                             annotate_peaks=False)),
-        ('periodic1Aw', models.periodic, dict(N=1000, seed=13, w=True),
-                                         dict(desc="periodic",
-                                             annotate_peaks=False)),
-
-        ]
-
-    for testdat in tests:
-        name, model = testdat[:2]
-        kwargs = kwargs_.copy()
-        modelargs = { }
-        if len(testdat) >= 3:
-            modelargs = testdat[2]
-        if len(testdat) >= 4:
-            kwargs.update(testdat[3])
+    for test in all_tests:
+        name = test.__name__
 
         # Skip tests we don't want to run, if we specify this thing.
         if to_run and name not in to_run:
             continue
+
 
         output = out_path+'test-'+name
         dirname = os.path.dirname(out_path)
         if dirname and not os.path.isdir(dirname):
             os.makedirs(dirname)
 
-        print name, model.func_name
-        run(name, model, output, modelargs=modelargs, kwargs=kwargs)
+        obj = test()
+        print name, obj.m.func_name
+        obj.run(output=output, **kwargs)
